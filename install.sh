6a462ceed774294f81b222a5b102cfd2cd572c00
Shivesh Chandra <shivesh96@gmail.com>
2021-12-28 14:51:02 +0530

    enable all command

--- install.sh
+++ install.sh
@@ -2,19 +2,24 @@ echo "***Cybolite Squid Proxy Installer***"
 echo "Updating System...."
 sudo apt update -y && upgrade -y
 echo "Installing Proxy"
-#sudo apt install squid3
+read -p "Set Proxy Port : " proxy_port
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+sudo apt install squid3
 echo "Enabling Firewall..."
-#sudo ufw enable
+sudo ufw enable
 echo "Configuring Squid to port 3128"
-#sudo ufw allow OpenSSH
-#udo ufw allow Squid
-#sudo ufw allow 3128
+sudo ufw allow OpenSSH
+udo ufw allow Squid
+#allow proxy_port
+sudo ufw allow 3128
 echo "Add 'cache deny all' just before 'http_access deny all'"
-echo "change 'http_access deny all' to 'http_access allow all'"
+echo "change 'http_access deny all' to 'http_access allow all' just after line '#  And finally deny all other access to this proxy'"
 echo "opening file..."
 sleep 5
 sudo nano /etc/squid/squid.conf
+sed -i 's/http_access deny all/http_access allow all/' /etc/squid/squid.conf
 echo "Reloading Services, Please weit..."
 sudo ufw reload
-#sudo systemctl restart squid.service
+sudo systemctl restart squid.service
+sudo systemctl status squid.service
 echo "Done"
